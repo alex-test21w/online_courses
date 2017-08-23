03bd77430e55567c0ac977ffd15bd9bed1e5e9c3
Alexey Chebotarev <alexey.chebotarev21@gmail.com>
2017-08-23 19:47:26 +0300

    Fix fonts for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.serve_static_assets = true
+  config.assets.compress = true
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -75,6 +79,10 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
