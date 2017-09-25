af56a4a9bc9946e073b384625d6e5a53b2495b94
Alexey Chebotarev <alexey.chebotarev21@gmail.com>
2017-09-25 17:55:05 +0300

    Add ffaker to seeds

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,7 +8,7 @@ class Course < ApplicationRecord
 
   has_many :course_users
   has_many :participants, -> { where('course_users.subscription = ?', true) }, through: :course_users, source: :user
-  has_many :lessons
+  has_many :lessons, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 100 }
 
